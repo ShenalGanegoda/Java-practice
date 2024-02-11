ad009c7e23edac17e5bf315e5333186ea0fdee3c
ShenalGanegoda <106686816+ShenalGanegoda@users.noreply.github.com>
2024-02-11 15:49:21 +0530

    2D arrays practice

---
+++ src/MultiDimensionalArrayPractice.java
@@ -0,0 +1,28 @@
+public class MultiDimensionalArrayPractice {
+
+    /*
+    Run on the main method:
+
+    int[][] my2DArray =
+            {
+
+                    {1 , 4 , 8},
+                    {3 , 4 , 6},
+                    {3 , 0 , 6}
+
+            };
+
+    MultiDimensionalArrayPractice.multiDimesionalArrayPrint(my2DArray);
+
+     */
+
+    public static void multiDimesionalArrayPrint(int[][] my2DArray){
+        for (int i = 0; i < my2DArray.length; i++) {
+            System.out.println();
+            for (int j = 0; j < my2DArray[i].length; j++) {
+                System.out.print(my2DArray[i][j] + " ");
+            }
+        }
+
+    }
+}
