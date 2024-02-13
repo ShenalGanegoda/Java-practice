ef7aa60f301e5deef2d14f962d8831de328ea94b
ShenalGanegoda <106686816+ShenalGanegoda@users.noreply.github.com>
2024-02-13 08:39:32 +0530

    Duplicate with one for loop exercise complete

--- src/DuplicateFindWithOneForLoop.java
+++ src/DuplicateFindWithOneForLoop.java
@@ -1,14 +1,15 @@
 public class DuplicateFindWithOneForLoop {
-    public int[] array;
 
-    public DuplicateFindWithOneForLoop(int[] array){
-        this.array = array;
-    }
-
-    public DuplicateFindWithOneForLoop(){}
-    public static int duplicateWithOneLoop(){
+    public static int duplicateWithOneLoop(int[] array){
         // Finding duplicates using only one for loop exercise.
+
         int duplicate = 0;
+        for (int i = 0; i < array.length; i++) { // {12 , 23 , 5 , 75 , 12 , 7}
+            // Traversing through the array.
+            if (array[0] == array[i]){ // Comparing adjacent elements in the array.
+                duplicate = array[i]; // Fixing the value to duplicate variable in the iteration.
+            }
+        }
         return duplicate;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        int[] array = {12 , 23 , 5 , 75 , 12 , 7};
+        DuplicateFindWithOneForLoop duplicate = new DuplicateFindWithOneForLoop();
+        System.out.println("Duplicate of the Array: " + duplicate.duplicateWithOneLoop(array));
+    }
 
 
 
