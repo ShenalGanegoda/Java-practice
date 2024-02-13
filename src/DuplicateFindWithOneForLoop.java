9d0316618cfe070ba534e2a2847cc6d0ac5b6dad
ShenalGanegoda <106686816+ShenalGanegoda@users.noreply.github.com>
2024-02-13 08:25:26 +0530

    Finding duplicate using only one for loop - Start

---
+++ src/DuplicateFindWithOneForLoop.java
@@ -0,0 +1,14 @@
+public class DuplicateFindWithOneForLoop {
+    public int[] array;
+
+    public DuplicateFindWithOneForLoop(int[] array){
+        this.array = array;
+    }
+
+    public DuplicateFindWithOneForLoop(){}
+    public static int duplicateWithOneLoop(){
+        // Finding duplicates using only one for loop exercise.
+        int duplicate = 0;
+        return duplicate;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {}
 
-    public static int duplicateWithOneLoop(){
-        // Finding duplicates using only one for loop exercise.
-        int duplicate = 0;
-        return duplicate;
-    }
+
 
     public static int[] mergeSort(int[] myArray){ //Merge sort approach.
         int[] sortedArray = new int[myArray.length];
