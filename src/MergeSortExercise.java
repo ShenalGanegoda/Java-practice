baebefa1fb797a1a7bdae22c7a1fbf4c29f1853c
ShenalGanegoda <106686816+ShenalGanegoda@users.noreply.github.com>
2024-02-18 15:23:28 +0530

    MergeSort Implementation start

--- src/MergeSortExercise.java
+++ src/MergeSortExercise.java
@@ -1,9 +1,16 @@
 public class MergeSortExercise {
-//    public static int[] mergeSort(){
-//        int[] myArray = {2 , 45  , 5 , 1 , 56 , 6 , 21 , 7};
-//
-//        for (int i = 0; i < myArray.length; i++) {
-//
-//        }
-//    }
+
+    public static void mergeSort(){
+        int[] myArray = { 2 , 45  , 5 , 1 , 56 , 6 , 21 , 7 , 6};
+        int midPoint = myArray.length / 2;
+        
+        int[] leftArray = new int[midPoint];
+        int[] rightArray = new int[myArray.length - midPoint];
+
+        for (int i = 0; i < myArray.length; i++) {
+            if (i < midPoint){ // Breaking the Array into Left side
+                leftArray[i] = myArray[i]; // Assigning the value to
+            } else rightArray[i] = myArray[i];
+        }
+    }
 }
