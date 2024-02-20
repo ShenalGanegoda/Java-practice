96bd27c71f42091059604c1f195c4dca9058b999
ShenalGanegoda <106686816+ShenalGanegoda@users.noreply.github.com>
2024-02-20 07:43:34 +0530

    Linked lists adding check

--- src/LinkedListsPractice.java
+++ src/LinkedListsPractice.java
@@ -3,4 +3,13 @@ import java.util.LinkedList;
 public class LinkedListsPractice {
     public LinkedList<String> mylinkedLists = new LinkedList<String>();
 
+    public void addElementsToLinkedList(){
+        mylinkedLists.push("A");
+        mylinkedLists.push("B");
+        mylinkedLists.push("C");
+        mylinkedLists.push("D");
+        mylinkedLists.push("E");
+
+        System.out.println(mylinkedLists);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        LinkedListsPractice addingCheck = new LinkedListsPractice();
+        addingCheck.addElementsToLinkedList();
+    }
 
 
 
