2d8eb0b993acfb03d5788c8156cec52d4b157a1a
ShenalGanegoda <106686816+ShenalGanegoda@users.noreply.github.com>
2024-03-02 12:00:05 +0530

    String compression - Hackathon exercise start

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(firstOccurrenceIndex("butsad" , "sad"));
+        String str = "aabcccccaaa";
+        System.out.println(stringCompression(str));
     }
 
 
@@ -14,13 +15,36 @@ public class Main {
 
 
     /*
+
+    Input: "aabcccccaaa"
+    Output: "a2b1c5a3"
+
+    String compression
+    -
+
+    Write a function that performs basic string compression using the courts of repeated characters.
+    For example, the string "aabcceccaaa" would become "a2b1c5a3". If the "compressed" string is not smaller than the
+    original string, your function should return the original string.
+    You can assume that the string only contains uppercase and lowercase letters (a-z, A-Z).
+
+    */
+    public static String stringCompression(String str){ //"aabcccccaaa"
+        String newString = "";
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) != str.charAt(i + 1)) {
+                newString = str.charAt(i) + Integer.toString(i);
+            }
+        } return newString;
+    }
+    public static int firstOccurrenceIndex(String haystack , String needle){ // Haystack - sadbutsad / Needle - sad
+
+            /*
     Finding the index of the first occurrence in a String.
 
     Given two strings needle and haystack, return the index of the first occurrence of needle in haystack,
     or -1 if needle is not part of haystack.
     */
 
-    public static int firstOccurrenceIndex(String haystack , String needle){ // Haystack - sadbutsad / Needle - sad
         int firstOccurenceIndex = 0; //Variable to hold the index.
         for (int i = 2; i < haystack.length(); i++) {
             if (needle.equals(""+ haystack.charAt(i - 2) + haystack.charAt(i - 1) + haystack.charAt(i))){
